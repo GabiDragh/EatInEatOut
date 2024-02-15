934750db85549d1d0adb41912553e3ededa9c1ff
Gabi Draghici <draghici.gabi89@gmail.com>
2024-02-15 00:00:43 +0000

    Api call working but searchInput variable not working

--- assets/js/EatIn-script.js
+++ assets/js/EatIn-script.js
@@ -1,12 +1,12 @@
-
-
-var formInput = document.getElementsByClassName("form-control").value;
-// API call
-
-// Spoontacular API key
+// Define variable that stores the user input value
+var searchInput = $("#foodSearchInput").val();
+console.log(searchInput);
+// Make API call:
+// Define variable to store Spoontacular API key -- not needed, using RAPID API key
 var ApiKey = "782ee6e2b6844a61ab8eb474f3479b22"; 
 
-var getRecipeUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=" + formInput + "&cuisine=" + formInput;
+
+var getRecipeUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=" + searchInput;
 var options = {
 	method: 'GET',
 	headers: {
@@ -16,14 +16,21 @@ var options = {
 };
 console.log(getRecipeUrl);
 
-fetch (getRecipeUrl, options)
+function getFoodInfo() {
+ fetch (getRecipeUrl, options)
 .then (function (response) {
-	console.log(JSON.stringify(response.json));
 	return response.json();
 })
 .then (function (recipeData) {
 	console.log(recipeData);
+    var foodArray = recipeData.results;
+	console.log(foodArray);
 });
+}
+
+// event listener for search button to trigger getRecipe function
+document.getElementById("button-addon2").addEventListener("click", getFoodInfo);
+
 // try {
 // 	const response = await fetch(getRecipeUrl, options);
 // 	const result = await response.text();
@@ -32,6 +39,6 @@ fetch (getRecipeUrl, options)
 // 	console.error(error);
 // }
 
-// event listener for search button to trigger getRecipe function
+
 //getRecipe function to include name, 
 // excludeIngredients parameter in API - can trigger when 6mo/1yo/2yo buttons are pressed to exclude certain ingredients from recipes
